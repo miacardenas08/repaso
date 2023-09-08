7b9398fe67e41e76c150fef31c128915decb8984
miacardenas08 <136343840+miacardenas08@users.noreply.github.com>
2023-09-08 07:46:31 -0300

    Add files via upload

---
+++ cantidades.js
@@ -0,0 +1,11 @@
+let cantidad_libros = 11
+let cantidad_frutas
+
+cantidad_libros = cantidad_libros + 5
+cantidad_frutas = 17
+
+console.log ('La cantidad de libro es' + cantidad_libros)
+
+//alert('Actualmente tenemos disponible' + cantidad_frutas + 'frutas')
+
+prompt('')
